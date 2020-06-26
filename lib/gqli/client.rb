2cbd50069c4ce4d634ff968a0929407f71ae7b38
dharmesh <dharmesh.convegenius@gmail.com>
2020-06-26 14:40:34 +0530

    added timeout configuration

--- lib/gqli/client.rb
+++ lib/gqli/client.rb
@@ -9,14 +9,18 @@ require_relative './version'
 module GQLi
   # GraphQL HTTP Client
   class Client
-    attr_reader :url, :params, :headers, :validate_query, :schema
+    attr_reader :url, :params, :headers, :validate_query, :schema, :options
 
-    def initialize(url, params: {}, headers: {}, validate_query: true)
+    def initialize(url, params: {}, headers: {}, validate_query: true, options: {})
       @url = url
       @params = params
       @headers = headers
       @validate_query = validate_query
-
+      @options = options
+      @options[:read_timeout] ||= 60
+      @options[:write_timeout] ||= 60
+      @options[:connect_timeout] ||= 60
+      
       @schema = Introspection.new(self) if validate_query
     end
 
@@ -34,7 +38,13 @@ module GQLi
     # Executres a query
     # Ignores validations
     def execute!(query)
-      http_response = HTTP.headers(request_headers).post(@url, params: @params, json: { query: query.to_gql })
+      http_response = HTTP.headers(request_headers)
+                          .timeout(
+                            :write => options[:write_timeout],
+                            :connect => options[:connect_timeout],
+                            :read => options[:read_timeout]
+                          )
+                          .post(@url, params: @params, json: { query: query.to_gql })
 
       fail "Error: #{http_response.reason}\nBody: #{http_response.body}" if http_response.status >= 300
 
