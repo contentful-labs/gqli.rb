eea484a6c19483182bd0fe018c185af98ba71e15
Smit Patel <smitpatel24@gmail.com>
2021-06-02 17:41:25 +0530

    Add support for timeouts (#11)
    * added timeout configuration
    
    * timeout options method added
    
    * identation fix
    
    * tests added for timeout config
    
    Co-authored-by: dharmesh <dharmesh.convegenius@gmail.com>

--- lib/gqli/client.rb
+++ lib/gqli/client.rb
@@ -9,13 +9,17 @@ require_relative './version'
 module GQLi
   # GraphQL HTTP Client
   class Client
-    attr_reader :url, :params, :headers, :validate_query, :schema
+    attr_reader :url, :params, :headers, :validate_query, :schema, :options
 
-    def initialize(url, params: {}, headers: {}, validate_query: true)
+    def initialize(url, params: {}, headers: {}, validate_query: true, options: {})
       @url = url
       @params = params
       @headers = headers
       @validate_query = validate_query
+      @options = options
+      @options[:read_timeout] ||= 60
+      @options[:write_timeout] ||= 60
+      @options[:connect_timeout] ||= 60
 
       @schema = Introspection.new(self) if validate_query
     end
@@ -34,7 +38,7 @@ module GQLi
     # Executres a query
     # Ignores validations
     def execute!(query)
-      http_response = HTTP.headers(request_headers).post(@url, params: @params, json: { query: query.to_gql })
+      http_response = request.post(@url, params: @params, json: { query: query.to_gql })
 
       fail "Error: #{http_response.reason}\nBody: #{http_response.body}" if http_response.status >= 300
 
@@ -50,6 +54,10 @@ module GQLi
       return true unless validate_query
 
       schema.valid?(query)
+    end
+
+    def request
+      HTTP.headers(request_headers).timeout(timeout_options)
     end
 
     protected
@@ -68,6 +76,14 @@ module GQLi
         accept: 'application/json',
         user_agent: "gqli.rb/#{VERSION}; http.rb/#{HTTP::VERSION}"
       }.merge(@headers)
+    end
+
+    def timeout_options
+      {
+        write: options[:write_timeout],
+        connect: options[:connect_timeout],
+        read: options[:read_timeout]
+      }
     end
   end
 end
